d69c2b041d1681a26289026d0b1e1c5f75e25a50
DafyddEvans2813865 <evansdafydd2002@gmail.com>
2024-05-04 22:57:15 +0100

    users now linked automatically to profile

--- TwitterClone/Tclone/models.py
+++ TwitterClone/Tclone/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,4 +8,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
+#force profile to be made on user sign up 
+def create_profile(sender, instance, created, **kwargs):
+    #when instance created assign it to profiles user feild
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+        #follow self and myself
+        user_profile.follows.set([instance.profile.id,User.objects.get(username='evans').id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
